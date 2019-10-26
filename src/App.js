4a0e740f9fdd16af5d573137b71aa8496b9f9677
Suwicha Saetie <suwicha2536@gmail.com>
2019-10-26 17:50:53 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom'
 import './App.css';
+
+import DashBorad from './Page/Dashborad'
+import Sidebar from './Component/Sidebar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App container">
+        <Route path="/" component={DashBorad} exact/>
+        <Route path="/Sidebar" component={Sidebar} />
+      </div>
     </div>
   );
 }
---
+++ src/Component/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import './Component/CSS/Sidebar.scss'
+
+export default class Sidebar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className='Sidebar'>
+                Sidebar
+            </div>
+        )
+    }
+}
